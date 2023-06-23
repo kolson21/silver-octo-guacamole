11d3fff212232fb28e87cb90f01172b77af8aae7
Kim Olson <kolson622@gmail.com>
2023-06-23 16:40:11 -0600

    working with files in node

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+// Always use asynchronous methods
+const fs = require('node:fs')
 
-const os = require('node:os');
+const files = fs.readdirSync('./')
+console.log(files)
 
-var totalMemory = os.totalmem()
-var freeMemory = os.freemem()
-
-// console.log('Total Memory: ' + totalMemory)
-
-// Template string
-// ES6 / ES2015 : ECMAScript 6
-
-console.log(`Total Memory: ${totalMemory}`)
-console.log(`Free Memory: ${freeMemory}`)
+fs.readdir('$', function(err, files) {
+    if (err) console.log('Error', err)
+    else console.log('Result', files)
+})
